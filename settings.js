bddd71d1f53fd85dd78ff3d11dbb67c668fa8e5b
yoelvy <gantengyoel78@gmail.com>
2022-04-16 11:07:26 +0700

    yoelvy

---
+++ settings.js
@@ -0,0 +1,68 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+global.APIs = {
+	webnya: 'https://api.lolhuman.xyz',
+}
+
+global.APIKeys = {
+	'https://api.lolhuman.xyz': 'Deffbotz',
+}
+
+global.owner = ['6285781375237']
+global.premium = ['6285781375237']
+global.packname = 'Bot'
+global.author = 'By noverbotz'
+global.sessionName = 'kirbotz'
+global.namabotnya = 'noverbotz'
+global.namaownernya = 'noverbotz'
+global.lolkey = 'Deffbotz'
+global.wame = 'https://wa.me/6285781375237'
+global.gckirbotz = ' - '
+global.ownerNumber = ["6285781375237@s.whatsapp.net"]
+global.email = ' - '
+global.yt = ' - '
+global.region = 'Indonesia'
+global.prefa = ['','!','.','#','-','•']
+global.sp = '⭔'
+global.ya = '```'
+global.mess = {
+    success: 'Success 💯',
+    admin: 'Fitur Khusus Admin Group!!!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!!!',
+    owner: 'Fitur Khusus Owner Bot!!!',
+    group: 'Fitur Digunakan Hanya Untuk Group!!!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!!!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot!!!',
+    error: 'Error Kak Silahkan Chat Owner',
+    wait: '❗Waittt...',
+    noPetualang: 'Itssss Kamu Belum Petualang Silahkan Ketik .joinrpg <namamu>',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+
+
+global.limitawal = {
+    premium: "Infinity",
+    free: 5
+}
+
+
+global.thumb = fs.readFileSync('./Image/kirbotz.jpg')
+global.thumbrpg = fs.readFileSync('./Image/kirbotzrpg.jpg')
+global.imgjoinrpg = fs.readFileSync('./Image/joinrpg.jpg')
+global.imgmining = fs.readFileSync('./Image/mining.jpg')
+
+global.imgdevil = fs.readFileSync('./Image/img/devil.jpg')
+global.imgslime = fs.readFileSync('./Image/img/slime.jpg')
+global.imgdemon = fs.readFileSync('./Image/img/demon.jpg')
+global.imggoblin = fs.readFileSync('./Image/img/goblin.jpg')
+global.imgdemonking = fs.readFileSync('./Image/img/demonking.jpg')
+global.imgbehemoth = fs.readFileSync('./Image/img/behemoth.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
